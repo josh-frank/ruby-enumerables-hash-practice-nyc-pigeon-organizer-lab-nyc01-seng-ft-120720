6a96abb9a2e524b3649d3167b1c9118458311a55
Joshua Frank <eurosewusa@gmail.com>
2020-11-13 13:30:16 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  result = data.each_with_object({}) do |( key, values ), resultArray|
-    values.each do |innerKey, namesArray|
+  result = data.each_with_object({}) do | ( key, values ), resultArray |
+    values.each do | innerKey, namesArray |
       namesArray.each do |name|
         resultArray[ name ] = {} if !resultArray[ name ]
         resultArray[ name ][ key ] = [] if !resultArray[ name ][ key ]
